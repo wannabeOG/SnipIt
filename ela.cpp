573a864e31b8183aa5ae3d084c27990a118895bb
Himal <saihimallu1010@gmail.com>
2018-04-09 22:36:01 +0000

    Initial commit

---
+++ ela.cpp
@@ -0,0 +1,19 @@
+// Author : Saihimal Allu 
+
+// Usage : The following code is an implementation of the ELA algorithm 
+// in a JPEG image. This implementation was done as a part of an assig-
+// ment for RedCarpet. 
+
+
+// Importing the libraries 
+# include <iostream>
+# include <opencv2/highgui/highgui.hpp>
+# include "opencv2/imgproc.hpp"
+using namespace std;
+using namespace cv;
+
+ int main() {
+
+ 	cout << "Hello world" << endl;
+ 	return 0;
+ }
