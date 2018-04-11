a492c6d02447b2f142149bc9ac55d8740cdd83b7
Himal <saihimallu1010@gmail.com>
2018-04-11 02:56:31 +0000

    Final fully working implementation of the algorithm

--- ela.cpp
+++ ela.cpp
@@ -1,3 +1,11 @@
+// File: ela.cpp
+// Author: Saihimal Allu
+// Usage: The following module is a basic implementation of Error Level 
+// Analysis (ELA) algorithm to detect any external changes made to a 
+// image through image editing software like Photoshop 
+
+// Importing the libraries
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -7,39 +15,41 @@
 using namespace cv;
 using namespace std;
 
-Mat ela_implementation(Mat image, int compression_factor) {
+Mat ela_implementation(Mat image, int compression_factor, int multiplier) {
 
  // Takes in the image, compresses the image and returns the result
-   cout << image.channels() << endl;
+ // cout << image.channels() << endl;
     std::vector <int> params;
     params.push_back(CV_IMWRITE_JPEG_QUALITY);
     params.push_back(compression_factor);
     Mat comp_image;
     Mat comp_image2; 
-    //cv::cvtColor(image, image, CV_BGR2RGB);
+ // cv::cvtColor(image, image, CV_BGR2RGB);
     comp_image = cv::imwrite("compressed.jpg",image,params);
-    
+ // Done so as to ensure that the imwrite function which reduces the
+ // number of channels can be boosted up to the original number   
     comp_image2 = cv::imread("compressed.jpg", CV_LOAD_IMAGE_COLOR);
-    cout << comp_image2.channels() << endl;
-    // Split the image into different channels RGB
+ //  cout << comp_image2.channels() << endl;
+
+ // Split the image into different channels RGB
     Mat uncomp_bgr[3];
     Mat comp_bgr[3];
     split(image,uncomp_bgr);
     split(comp_image2, comp_bgr);
     
-    // Stores the difference between the components of channels of comp
-    // ressed and uncompressed image  
+ // Stores the difference between the components of channels of comp
+ // ressed and uncompressed image  
     Mat diff[3];
-    //blue channel difference 
+ // Blue channel difference 
     absdiff(comp_bgr[0],uncomp_bgr[0],diff[0]);
-    // Green channel difference 
+ // Green channel difference 
     absdiff(comp_bgr[1],uncomp_bgr[1],diff[1]);
-    // Red channel difference
+ // Red channel difference
     absdiff(comp_bgr[2],uncomp_bgr[2],diff[2]);
 
-    // Combine the channels of the diff object to get the difference image
+ // Combine the channels of the diff object to get the difference image
 
-    // Final image declaration 
+ // Final image declaration 
     Mat final;
     Mat diff_img;
     std::vector <Mat> channels;
@@ -47,41 +57,63 @@ Mat ela_implementation(Mat image, int compression_factor) {
     channels.push_back(diff[1]);
     channels.push_back(diff[2]);
 
+ // Merge the single-array channels into a BGR image  
     merge(channels,final);
 
-    // Storing the diff image
+ // Storing the diff image
     imwrite("diff.jpg", final);
-    //   Mat diff_img;
-    diff_img = imread("diff.jpg" ,1);
-    namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
-    imshow( "Display window", diff_img );  
-    return diff_img;
+    
+    Mat diff_image = imread("diff.jpg",CV_LOAD_IMAGE_COLOR);
+
+    for (int i = 0; i< image.rows; ++i) {
+
+        uchar* p = diff_image.ptr <uchar> (i);
+
+        for (int j = 0; j<image.cols; ++j) {
+
+            for (int Channel = 0; Channel <3; ++Channel) {
+
+                // Thresholdig operation 
+                if(p[Channel]*multiplier>255) p[Channel] = 255;
+                // Scaling the differeces
+                else p[Channel] = p[Channel]*multiplier;
+            }
+
+            p+=3;
+        } 
+    }
+ // 
+ // diff_img = imread("diff.jpg" ,1);
+ // namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
+ // imshow( "Display window", diff_img );  
+    return diff_image;
        
 }
  
 int main(int argc, char** argv) {
-    //int multiplier=30;
+    
     int compression_value=75;
-    Mat img;
+    int multiplier=30;
+    Mat image;
     /*running ela on original image taken by mobile*/
-    img = imread(argv[1],CV_LOAD_IMAGE_COLOR);
+    image = imread(argv[1],CV_LOAD_IMAGE_COLOR);
     //cv::Size s = img.size();
     //int rows = s.height;
     //nt cols = s.width;
     //cout << rows << " " << cols <<endl;
     //cout << "Hello World" << endl;
-    if (img.empty()) {
+    
+    Mat elaResult_img;
+    
+    if (image.empty()) {
         cout << "could not load image !" << endl;
     } 
-    
-    Mat ela_result_img;
-    ela_result_img = ela_implementation(img,compression_value);
-    //imwrite("result_original_img.jpg",ela_result_img);
 
-    /*running ela on photoshopped image which have a logo added in original image saved at 100% quality jpeg*/
-    //Mat edited_img;
-    //edited_img=imread("photoshopped_img.jpg");
-    //Mat ela_result_edited_img=ela_algorithm(edited_img,compression_value, multiplier);
-    //imwrite("result_edited_img.jpg",ela_result_edited_img);
+     else{
+        elaResult_img = ela_implementation(image,compression_value,multiplier);
+    }    
+    
+    imwrite("ela_implemented_img.jpg",elaResult_img);
+
     return 0;
 }
